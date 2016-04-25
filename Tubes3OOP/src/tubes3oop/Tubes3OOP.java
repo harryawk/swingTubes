afe27eac25c6426e36e204e41c0b7a7c5d38dbc1
monstergelo <bimawansatrianto@gmail.com>
2016-04-25 12:49:13 +0700

    bisa print

--- Tubes3OOP/src/tubes3oop/Tubes3OOP.java
+++ Tubes3OOP/src/tubes3oop/Tubes3OOP.java
@@ -5,6 +5,10 @@
  */
 package tubes3oop;
 
+import WorldOfPaint.*;
+import Movement.*;
+import Organism.*;
+
 /**
  *
  * @author Ali-pc
@@ -14,8 +18,42 @@ public class Tubes3OOP {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+    public static void main(String[] args)
+	{
+		World w = new World();
+
+                Organisme o1 = new Eater();
+		Organisme o2 = new Eater();
+		Organisme o3 = new Eater();
+                
+		Point p1 = new Point(50, 50);
+		Point p2 = new Point(500, 500);
+		Point p3 = new Point(350, 700);
+                
+                o1.setPosisi(p1);
+                o2.setPosisi(p2);
+                o3.setPosisi(p3);
+
+		w.add(o1);
+		w.add(o2);
+		w.add(o3);
+
+		//game-loop
+		while(true)
+		{
+			w.updateGame();
+			w.updateDisplay();
+			
+
+			try
+	        {
+	            Thread.sleep(166);
+	        }
+	        catch(Exception e)
+	        {
+
+	        }
+		}
+	}
     
 }
